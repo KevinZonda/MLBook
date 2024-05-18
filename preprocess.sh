2760be3396162b4bf8e0284a9e3f1acf6ec712c7
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-05-18 03:54:27 +0100

    fix

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,2 @@
-gsed -i 's/\[!WARNING\]\s*/**⚠️ 警告**：/g' ./README.md
-gsed -i 's/\[!NOTE\]\s*/**ℹ️ 提醒**：/g' ./README.md
+sed -i 's/\[!WARNING\]\s*/**⚠️ 警告**：/g' ./README.md
+sed -i 's/\[!NOTE\]\s*/**ℹ️ 提醒**：/g' ./README.md
