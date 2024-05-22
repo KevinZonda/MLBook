9152b79524233b1a33f895780b99d42e240bdd0d
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-05-23 00:55:38 +0100

    fix

--- preprocess.sh
+++ preprocess.sh
@@ -3,3 +3,6 @@ sed -i 's/\[!NOTE\]\s*/**ℹ️ 提醒**：/g' ./README.md
 
 rm -fr code
 rm -fr .github
+
+wget https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js
+wget https://raw.githubusercontent.com/badboy/mdbook-mermaid/main/src/bin/assets/mermaid-init.js
