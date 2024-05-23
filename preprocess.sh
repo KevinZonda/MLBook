ef678548da827665a0b5e70a2e1f7273b0528661
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-05-23 15:18:50 +0100

    try

--- preprocess.sh
+++ preprocess.sh
@@ -11,3 +11,5 @@ mkdir theme
 cd theme
 wget https://raw.githubusercontent.com/slowsage/mdbook-pagetoc/main/src/pagetoc.css
 wget https://raw.githubusercontent.com/slowsage/mdbook-pagetoc/main/src/pagetoc.js
+wget https://raw.githubusercontent.com/tommilligan/mdbook-admonish/294af2478c7876874d4b0afe536dfaaa9036745f/src/bin/assets/mdbook-admonish.css
+cd ..
